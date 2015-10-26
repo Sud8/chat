f2acd6f862ec81409980820548bf54b2b887358e
Sudarshan Prabhu <sudarshan.prabhu@redbus.in>
2015-10-27 04:55:17 +0530

    changesnew

--- app.js
+++ app.js
@@ -28,13 +28,15 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-//app.use('/', routes);
+app.use('/', function(req, res) {
+  res.sendFile(path.join(__dirname, '', 'index.html'));
+});
 //app.use('/users', users);
 //app.use('/awesomeChat',awesome);
 
-app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, '', 'index.html'));
-});
+//app.get('/', function(req, res) {
+  //res.sendFile(path.join(__dirname, '', 'index.html'));
+//});
 
 
 io.sockets.on('connection',function(socket){
