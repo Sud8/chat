3c2918ae265b990ccf9d53df0ffeedcb4bf5f513
Sudarshan Prabhu <sudarshan.prabhu@redbus.in>
2015-10-28 15:45:18 +0530

    logging the chat messages in mongodb

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var awesome = require('./routes/awesome');
+var mongoose = require('mongoose');
 
 var app = express();
 var server = require('http').createServer(app),
@@ -15,6 +16,16 @@ io = require('socket.io').listen(server);
 
 var userNames = [];
 server.listen(process.env.PORT || 3000);
+
+
+var chatSchema = mongoose.Schema({
+  nick : String,
+  msg :String,
+  created :{type:Date,default:Date.now}
+});
+
+
+var chat = mongoose.model('chatpanda',chatSchema);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -36,10 +47,27 @@ app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '', 'index.html'));
 });
 
+mongoose.connect('mongodb://chatpanda:chatpanda@ds045704.mongolab.com:45704/message',function(err){
+  if(!err){
+    console.log('Connected to mongo db');
+  }
+  else
+  {
+    console.log(err);
+    console.log('Connection failed to mongo db');
+  }
+})
+
 
 io.sockets.on('connection',function(socket){
   //onsole.log("new message");
  
+    var query = chat.find({});
+    query.sort('-created').limit(8).exec(function(err,docs){
+    if(err) throw err;
+      socket.emit('send old messages',docs);
+  });
+
    function updateUserNames()
    {
       io.sockets.emit("username", userNames)
@@ -47,7 +75,15 @@ io.sockets.on('connection',function(socket){
 
   socket.on('send message',function(data){
        //console.log("new chat message");
-        io.sockets.emit('new message',{msg:data,nickName:socket.nickname});
+       var messg = new chat({msg:data,nick:socket.nickname});
+       messg.save(function(err){
+        if(err)
+        {
+          console.log(err);
+          throw err;
+        }
+          io.sockets.emit('new message',{msg:data,nickName:socket.nickname});
+        });
   });
 
   
